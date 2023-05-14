54d9ac45f2419ac9c014909a053fffbbe446cac9
Levi Porter <levi@levibrooke.com>
2023-05-13 23:31:39 -0700

    Add clerk key, update deps, remove patch

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,6 +1,7 @@
 import { ExpoConfig, ConfigContext } from "@expo/config";
 
-const CLERK_PUBLISHABLE_KEY = "your-clerk-publishable-key";
+const CLERK_PUBLISHABLE_KEY =
+  "pk_test_ZXhvdGljLWJvYS0xLmNsZXJrLmFjY291bnRzLmRldiQ";
 
 const defineConfig = (_ctx: ConfigContext): ExpoConfig => ({
   name: "expo",
