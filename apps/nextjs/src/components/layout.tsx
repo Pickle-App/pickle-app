1ceef864a7e8d3f3fcc85a243f87ddfda82f8551
Landon <lndncole@gmail.com>
2023-05-20 23:34:13 -0700

    conditionally render menu bar with profile, reduce padding on menu bar

--- apps/nextjs/src/components/layout.tsx
+++ apps/nextjs/src/components/layout.tsx
@@ -1,20 +1,33 @@
 import React from "react";
 import MenuBar from "./menubar";
+import { useAuth } from "@clerk/nextjs";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return (
-    <>
-      <div className="flex h-screen flex-col">
-        <MenuBar />
-        {children}
-      </div>
-      {/* Additional layout components or elements */}
-    </>
-  );
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return (
+      <>
+        <div className="flex h-screen flex-col">
+          <MenuBar />
+          {children}
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="flex h-screen flex-col">{children}</div>
+      </>
+    );
+  }
+
+  // Return null if the user is not signed in
+  return null;
 };
 
 export default Layout;
