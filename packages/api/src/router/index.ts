8d6b32e3262f07f42e8a217fe1e374f47f39cec2
Levi Porter <levi@levibrooke.com>
2024-01-07 16:34:39 -0800

    Update @trpc deps to 10.45, add back profile router (#7)

--- packages/api/src/router/index.ts
+++ packages/api/src/router/index.ts
@@ -1,10 +1,10 @@
 import { router } from "../trpc";
-import { postRouter } from "./post";
 import { authRouter } from "./auth";
+import { profileRouter } from "./profile";
 
 export const appRouter = router({
-  post: postRouter,
   auth: authRouter,
+  profile: profileRouter,
 });
 
 // export type definition of API
